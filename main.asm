9da3e1dc12473800460cf0fde29b50b07c768a13
mohamedabdallatif <66181537+mohamedabdallatif@users.noreply.github.com>
2022-12-06 23:08:46 +0200

    first screen

--- main.asm
+++ main.asm
@@ -1,55 +1,97 @@
-name "mycode"   ; output file name (max 8 chars for DOS compatibility)
+	STACK SEGMENT PARA STACK
+	DB 64 DUP (' ')
+STACK ENDS
 
-org  100h	; set location counter to 100h
+DATA SEGMENT PARA 'DATA'
+    TEXT_GAME_OVER_MAIN_MENU DB 'Press E to exit to main menu','$' ;text with the game over main menu message
+	TEXT_MAIN_MENU_TITLE DB 'MAIN MENU press 1','$' ;text with the main menu title
+	TEXT_MAIN_MENU_SINGLEPLAYER DB 'HELP press 2','$' ;text with the singleplayer message
+	TEXT_MAIN_MENU_EXIT DB 'EXIT press 3','$' ;text with the exit game message
+DATA ENDS
 
-; you may customize this and other start-up templates 
-; the location of this template is c:\emu8086\inc\0_com_template.txt
+    CODE SEGMENT PARA 'CODE'
 
-; disable blinking (for DOS/BIOS compatibility).
-mov ax, 1003h  
-mov bx, 0        
-int 10h
+	MAIN PROC FAR
+	ASSUME CS:CODE,DS:DATA,SS:STACK      ;assume as code,data and stack segments the respective registers
+	PUSH DS                              ;push to the stack the DS segment
+	SUB AX,AX                            ;clean the AX register
+	PUSH AX                              ;push AX to the stack
+	MOV AX,DATA                          ;save on the AX register the contents of the DATA segment
+	MOV DS,AX                            ;save on the DS segment the contents of AX
+	POP AX                               ;release the top item from the stack to the AX register
+	POP AX                               ;release the top item from the stack to the AX register
+		
+		CALL CLEAR_SCREEN                ;set initial video mode configurations
+        SHOW_MAIN_MENU:
+        CALL DRAW_MAIN_MENU
+        DRAW_MAIN_MENU PROC NEAR
+        CALL CLEAR_SCREEN
+    ;       Shows the menu title
+MOV AH,02h                       ;set cursor position
+MOV BH,00h                       ;set page number
+MOV DH,04h                       ;set row
+MOV DL,04h                         ;set column
+INT 10h
 
-; this is just a random code
-mov ax, 1234h      ; load ax with hexadecimal value.
-mov dx, 0x1234     ; 0x is a valid hexadecimal prefix too.
-mov ax, 1234       ; load ax with 04D2h = decimal 1234 = 10011010010b.
-mov ax, 0ABCDh     ; must have 0 prefix if first digit is A,B,C,D,E or F
-mov dx, 0xABCD     ;                                      or use 0x prefix.
-mov dl, TeSt1      ; the integrated 8086 assembler is not case sensitive, test1=TeSt1, AX=ax, etc...
-jmp n              ; jump over 1 byte to skip over the test1 variable.
-test1 db 20h       ; 1 byte variable 20h = 32.
-n:
-add dl, dl         ; dl = dl+dl = 20h+20h = 40h = 64 = 01000000b
-inc dl             ; dl = dl+1 = 41h = 65 = 'A' (ascii code)
-mov ah, 2          ; ah and dl are parameters for int 21h
-int 21h            ; print 'A'.
-mov dl, 1          ; ascii code 1 is a funny face.
-int 21h            ; print it.
-mov al, 11100101b  ; b suffix is for binary. 11100101b = 0xE5 = 229
-xor al, 1111_1110b ; _ can separate nibbles.
+MOV AH,09h                       ;WRITE STRING TO STANDARD OUTPUT
+LEA DX,TEXT_MAIN_MENU_TITLE      ;give DX a pointer
+INT 21h                          ;print the string
 
-; add your code here
+;       Shows the singleplayer message
+MOV AH,02h                       ;set cursor position
+MOV BH,00h                       ;set page number
+MOV DH,06h                       ;set row
+MOV DL,04h                         ;set column
+INT 10h
 
-mov dx, 0705h      ; print message using BIOS function at 7,5
-mov bx, 0          ; page 0.
-mov bl, 10011111b  ; white on blue.
-mov cx, msg_size   ; number of characters to print.
-mov al, 01b        ; update cursor only.
-mov bp, offset msg
-mov ah, 13h        ; BIOS function for print.
-int 10h            ; print message at es:bp.
-
-mov ah, 0          ; wait for any key....
-int 10110b         ; same as int 16h or int 22.
-
-ret   ; return to the operating system.
-
-msg         db "press any key..."
-msg_size =  $ - offset msg
-
-
+MOV AH,09h                       ;WRITE STRING TO STANDARD OUTPUT
+LEA DX,TEXT_MAIN_MENU_SINGLEPLAYER      ;give DX a pointer
+INT 21h                          ;print the string
 
 
 
+;       Shows the exit message
+MOV AH,02h                       ;set cursor position
+MOV BH,00h                       ;set page number
+MOV DH,08h                       ;set row
+MOV DL,04h                         ;set column
+INT 10h   
+MOV AH,09h                       ;WRITE STRING TO STANDARD OUTPUT
+		LEA DX,TEXT_MAIN_MENU_EXIT      ;give DX a pointer 
+		INT 21h  
+                        ;print the string
+MAIN_MENU_WAIT_FOR_KEY:
+;       Waits for a key press
+			MOV AH,00h
+			INT 16h
+		
+;       Check whick key was pressed
+			CMP AL,'1'
+			JE MAIN
+			
+			CMP AL,'2'
+			JE HELP
+			
+			CMP AL,'E'
+			JE EXIT
+			
+			JMP MAIN_MENU_WAIT_FOR_KEY	
+RET
 
+DRAW_MAIN_MENU ENDP
+CLEAR_SCREEN PROC NEAR               ;clear the screen by restarting the video mode
+MOV AH,00h                   ;set the configuration to video mode
+MOV AL,13h                   ;choose the video mode
+INT 10h                         ;execute the configuration
+MOV AH,00h                      ;set the configuration
+MOV BH,00h                      ;to the background color
+MOV BL,00h                      ;choose black as background color
+INT 10h                         ;execute the configuration
+RET
+CLEAR_SCREEN ENDP 
+
+MAIN_MENU:
+
+HELP:
+
+EXIT:
